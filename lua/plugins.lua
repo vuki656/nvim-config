e0c109c2737b0846ab5b1a5e5245cd64c1a9579c
domagoj.vukovic <off.vukovic@gmail.com>
2021-03-23 09:32:52 +0100

    Plugin manager, basic structure, file tree

---
+++ lua/plugins.lua
@@ -0,0 +1,20 @@
+------------------------------------------------------------------------------------------
+----------------------------------- BASE PACKER CONFIG -----------------------------------
+------------------------------------------------------------------------------------------
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  execute 'packadd packer.nvim'
+end
+
+------------------------------------------------------------------------------------------
+----------------------------------- LIST -------------------------------------------------
+------------------------------------------------------------------------------------------
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'kyazdani42/nvim-tree.lua'
+end)
---
+++ lua/remaps.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = ' '
+
+vim.api.nvim_set_keymap('n', '<Leader>n', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
