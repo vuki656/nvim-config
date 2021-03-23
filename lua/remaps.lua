60e6bfc3df961f257a4bee3d547b9220e5a6aff7
domagoj.vukovic <off.vukovic@gmail.com>
2021-03-23 20:19:36 +0100

    Converted sets

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -1,3 +1,23 @@
-vim.g.mapleader = ' '
+local remap = vim.api.nvim_set_keymap 
 
-vim.api.nvim_set_keymap('n', '<Leader>n', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+------------------------------------------------------------------------------------------
+----------------------------------- GENERAL ----------------------------------------------
+------------------------------------------------------------------------------------------
+
+-- Go to normal mode with jj
+remap('i', 'jj', '<ESC>', { noremap = true, silent = true })
+
+-- Buffer navigation
+remap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+remap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+remap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+remap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+
+
+------------------------------------------------------------------------------------------
+----------------------------------- FILE TREE --------------------------------------------
+------------------------------------------------------------------------------------------
+
+-- Toggle file tree
+remap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
---
+++ lua/sets.lua
@@ -0,0 +1,41 @@
+local option = vim.api.nvim_set_option
+local win_option = vim.api.nvim_win_set_option
+local global = vim.g
+
+------------------------------------------------------------------------------------------
+----------------------------------- GENERAL ----------------------------------------------
+------------------------------------------------------------------------------------------
+
+
+global.mapleader = ' '                              -- Set leader
+
+option('clipboard', 'unnamedplus')                  -- Sync system and nvim clipboards
+option('incsearch', true)                           -- Incremental search
+option('smartindent', true)                         -- Smart indenting
+option('signcolumn', 'yes')                         -- Always show sign column
+option('updatetime', 300)                           -- Update time
+option('scrolloff', 10)                             -- When 10 lines from window edge, start scrolling
+option('mouse', 'a')                                -- Enable mouse scrolling
+option('swapfile', false)                           -- Disable swap files
+option('undofile', true)                            -- Enable undo files
+option('scroll', 15)                                -- Scroll line amount
+option('tabstop', 4)                                -- Convert tab to 4 spaces
+option('shiftwidth', 4)                             -- Correct block indentation
+option('expandtab', true)                           -- Convert tabs to spaces
+option('splitright', true)                          -- Open new buffers to the right
+option('splitbelow', true)                          -- Open new buffers to the bottom
+
+win_option(0, 'cursorline', true)                   -- Don't wrap text
+win_option(0, 'wrap', false)                        -- Don't wrap text
+win_option(0, 'number', true)                       -- Display sidebar numbers
+win_option(0, 'relativenumber', true)               -- Display sidebar numbers relative 
+
+
+------------------------------------------------------------------------------------------
+----------------------------------- PLUGIN REQUIRED --------------------------------------
+------------------------------------------------------------------------------------------
+
+
+-- nvim-compee
+option('completeopt', 'menuone,noselect')
+
