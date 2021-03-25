f5c1cadd068e3ae0d738588669ca4d0043537b1f
Domagoj <off.vukovic@gmail.com>
2021-03-25 20:37:50 +0100

    LSP

--- init.lua
+++ init.lua
@@ -9,6 +9,17 @@ require('plugins.smoothie')
 require('plugins.lspsaga')
 require('plugins.compe')
 
-require('lsp.init')
-require('lsp.ts')
+require'lspconfig'.gopls.setup{}
+require('lsp.bash.lua')
+require('lsp.css.lua')
+require('lsp.diagnostics.lua')
+require('lsp.docker.lua')
+require('lsp.graphql.lua')
+require('lsp.html.lua')
+require('lsp.json.lua')
+require('lsp.lua.lua')
+require('lsp.ts.lua')
+require('lsp.vim.lua')
+require('lsp.yaml.lua')
+
 
--- lua/plugins/compe.lua
+++ lua/plugins/compe.lua
@@ -22,3 +22,10 @@ require'compe'.setup {
   };
 }
 
+vim.cmd([[
+    inoremap <silent><expr> <C-Space> compe#complete()
+    inoremap <silent><expr> <CR>      compe#confirm('<CR>')
+    inoremap <silent><expr> <C-e>     compe#close('<C-e>')
+    inoremap <silent><expr> <C-f>     compe#scroll({ 'delta': +4 })
+    inoremap <silent><expr> <C-d>     compe#scroll({ 'delta': -4 })
+]])
