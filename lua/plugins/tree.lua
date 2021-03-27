cb79b4dd16472037b5c4e0823492ec58b7aa3713
domagoj.vukovic <off.vukovic@gmail.com>
2021-03-27 10:47:19 +0100

    file tree icons

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -3,13 +3,19 @@
 ------------------------------------------------------------------------------------------
 
 vim.g.nvim_tree_indent_markers = 1                                   -- Display indent markers
-vim.g.nvim_tree_ignore = {".git", "node_modules", ".cache"}          -- Don't show listed firs
 vim.g.nvim_tree_git_hl = 1                                           -- Display git status in file names
+
+-- Don't show listed dirs
+vim.g.nvim_tree_ignore = {
+    ".git",
+    "node_modules",
+    ".cache"
+}    
 
 -- Custom icons 
 vim.g.nvim_tree_icons = {
-    default = " ",
-    symlink = " ",
+    default = "",
+    symlink = "",
     git = {
         unstaged = "✗",
         staged = "✓",
@@ -18,29 +24,29 @@ vim.g.nvim_tree_icons = {
         untracked = "★"
     },
     folder = {
-        default = "",
-        open = "",
+        default = "",
+        open = "",
         empty_open = "",
         empty = "",
         symlink = "",
     }
 }
 
--- Set where git appearsc
+-- Set where git appears
 vim.g.nvim_tree_show_icons = {
     git = 0,
     folders = 1,
     files = 1
 }
 
--- vim.cmd([[
---     highlight NvimTreeGitNew guifg=#437e54
---     highlight NvimTreeGitRenamed guifg=#ff8f00
---     highlight NvimTreeGitDirty guifg=#60aae0
---     highlight NvimTreeFolderName guifg=#a5afbe
---     highlight NvimTreeEmptyFolderName guifg=#a5afbe
---     highlight NvimTreeFolderIcon guifg=#a5afbe
--- ]])
+vim.cmd([[
+    highlight NvimTreeGitNew guifg=#98c379
+    highlight NvimTreeGitRenamed guifg=#ff8f00
+    highlight NvimTreeGitDirty guifg=#60aae0
+    highlight NvimTreeFolderName guifg=#a5afbe
+    highlight NvimTreeEmptyFolderName guifg=#a5afbe
+    highlight NvimTreeFolderIcon guifg=#a5afbe
+]])
 
 ------------------------------------------------------------------------------------------
 ----------------------------------- REMAPS -----------------------------------------------
