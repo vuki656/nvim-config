a4c9fed4659abe707ed1ed83f9c7f6bfd584ca2e
domagoj.vukovic <off.vukovic@gmail.com>
2021-06-06 18:07:36 +0200

    ref/smoothie api

--- lua/plugins/smoothie.lua
+++ lua/plugins/smoothie.lua
@@ -2,13 +2,11 @@
 -- Description: Smoother scrolling
 -- Link: https://github.com/psliwka/vim-smoothie
 
-local option = vim.api.nvim_set_var
-
 ------------------------------------------------------------------------------------------
 ------------------------------------ SETUP -----------------------------------------------
 ------------------------------------------------------------------------------------------
 
-option('smoothie_break_on_reverse', false)
-option('smoothie_base_speed', 20)
-option('smoothie_update_interval', 30)
-option('smoothie_speed_exponential_factor', 1.1)
+vim.g.smoothie_break_on_reverse = false
+vim.g.smoothie_base_speed = 20
+vim.g.smoothie_update_interval = 30
+vim.g.smoothie_speed_exponential_factor = 1.1
