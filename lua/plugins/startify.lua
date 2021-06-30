85e52ebecb876bf5a064a20e933ec3d5fc4d3d20
domagoj.vukovic <off.vukovic@gmail.com>
2021-06-30 20:07:02 +0200

    remove startify icons, add excluded folders to nvim tree

--- lua/plugins/startify.lua
+++ lua/plugins/startify.lua
@@ -22,13 +22,6 @@ vim.g.startify_lists = {
 -- Start list at 1
 vim.cmd("let g:startify_custom_indices = map(range(1,100), 'string(v:val)')")
 
--- Load icons
-vim.cmd([[
-    function! StartifyEntryFormat() abort
-        return 'v:lua.webDevIcons(absolute_path) . " " . entry_path'
-    endfunction
-]])
-
 ------------------------------------------------------------------------------------------
 ----------------------------------- REMAPS -----------------------------------------------
 ------------------------------------------------------------------------------------------
