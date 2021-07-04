3661b91d9fba844d96dbc12b9db4029aec12fd10
Domagoj <off.vukovic@gmail.com>
2021-07-04 21:00:11 +0200

    add auto confirm to install script packages

--- install.sh
+++ install.sh
@@ -5,16 +5,16 @@
 ################################################################################################ 
 
 install_lsps() {
-    npm i -g vscode-css-languageserver-bin
-    npm i -g bash-language-server
-    npm i -g diagnostic-languageserver
-    npm i -g dockerfile-language-server-nodejs
-    npm i -g graphql-language-service-cli
-    npm i -g vscode-html-languageserver-bin
-    npm i -g vscode-json-languageserver
-    npm i -g typescript typescript-language-server
-    npm i -g vim-language-server
-    npm i -g yaml-language-server
+    npm i -g vscode-css-languageserver-bin -y
+    npm i -g bash-language-server -y
+    npm i -g diagnostic-languageserver -y
+    npm i -g dockerfile-language-server-nodejs -y
+    npm i -g graphql-language-service-cli -y
+    npm i -g vscode-html-languageserver-bin -y
+    npm i -g vscode-json-languageserver -y
+    npm i -g typescript typescript-language-server -y
+    npm i -g vim-language-server -y
+    npm i -g yaml-language-server -y
 }
 
 ################################################################################################
@@ -22,7 +22,7 @@ install_lsps() {
 ################################################################################################ 
 
 install_nvim() {
-    sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip -y
+    sudo apt install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip -y
 
     cd ~/
     git clone https://github.com/neovim/neovim
