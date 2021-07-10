cf2429613f5d19c5805ad0b98da3bfde67911a50
domagoj.vukovic <off.vukovic@gmail.com>
2021-07-10 13:05:42 +0200

    document spelunker

--- lua/plugins/spellcheck.lua
+++ lua/plugins/spellcheck.lua
@@ -1,6 +1,21 @@
+-- Name: Spelunker
+-- Description: Spell check
+-- Link: https://github.com/kamykn/spelunker.vim
+
 local remap = vim.api.nvim_set_keymap
 
+------------------------------------------------------------------------------------------
+----------------------------------- REMAPS -----------------------------------------------
+------------------------------------------------------------------------------------------
+
+-- Fix current
 remap("n", "<leader>sf", "Zl", {})
+
+-- Exclude word
 remap("n", "<leader>sa", "Zg", {})
+
+-- Go to next mistake
 remap("n", "<leader>sn", "ZN", {})
+
+-- Go to previous mistake
 remap("n", "<leader>sp", "ZP", {})
