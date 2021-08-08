c44a9bf4f138da34aac75efeefe0322560497769
domagoj.vukovic <off.vukovic@gmail.com>
2021-08-08 12:01:13 +0200

    feat: package info keys and docs

--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -1,3 +1,30 @@
-require("package-info").setup({
-    autostart = false
+-- Name: Package Info
+-- Description: Display outdated package versions in package.json
+-- Link: https://github.com/vuki656/package-info.nvim
+
+local package_info = require("package-info")
+
+------------------------------------------------------------------------------------------
+----------------------------------- SETUP ------------------------------------------------
+------------------------------------------------------------------------------------------
+
+package_info.setup({
+    autostart = false,
 })
+
+------------------------------------------------------------------------------------------
+----------------------------------- REMAPS -----------------------------------------------
+------------------------------------------------------------------------------------------
+
+vim.api.nvim_set_keymap(
+    "n",
+    "<leader>nc",
+    ":lua require('package-info').clear()<CR>",
+    { silent = true, noremap = true }
+)
+vim.api.nvim_set_keymap(
+    "n",
+    "<leader>nd",
+    ":lua require('package-info').display()<CR>",
+    { silent = true, noremap = true }
+)
