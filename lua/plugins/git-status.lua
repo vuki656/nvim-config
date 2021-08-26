f35ac261df3295618454b854da6e5c7c0c5b9c76
Domagoj <off.vukovic@gmail.com>
2021-08-26 10:29:28 +0200

    fix: git signs

--- lua/plugins/git-status.lua
+++ lua/plugins/git-status.lua
@@ -43,9 +43,12 @@ git_signs.setup({
             linehl = "GitSignsChangeLn",
         },
     },
-    current_line_blame_opts = ({
+    current_line_blame = true,
+    current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol",
         delay = 300,
-    }),
+    },
 })
 
 ------------------------------------------------------------------------------------------
