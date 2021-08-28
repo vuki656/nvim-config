d599179f1bc450528a2d88eb17a8faef45ddda75
domagoj.vukovic <off.vukovic@gmail.com>
2021-08-28 23:34:41 +0200

    feat: toggle term mappings

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -17,6 +17,18 @@ toggleterm.setup({
 ----------------------------------- REMAPS -----------------------------------------------
 ------------------------------------------------------------------------------------------
 
+function _G.set_terminal_keymaps()
+    local opts = { noremap = true }
+    vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+end
+
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
 ----------------------------------- LAZYGIT ----------------------------------------------
 local lazygit = Terminal:new({
     cmd = "lazygit",
