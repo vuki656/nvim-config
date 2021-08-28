5799d7e5fad122bb5e227ce9b3180ffb97445da8
domagoj.vukovic <off.vukovic@gmail.com>
2021-08-28 23:24:50 +0200

    feat: basic popup terminal setup

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -27,10 +27,11 @@ local lazygit = Terminal:new({
     },
     on_open = function(terminal)
         vim.cmd("startinsert!")
-        vim.api.nvim_buf_set_keymap(terminal.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(terminal.bufnr, "n", "q", ":close<CR>", { noremap = true, silent = true })
     end,
     on_close = function()
         vim.cmd(":NvimTreeRefresh")
+        vim.cmd(":e")
     end,
     hidden = true,
 })
@@ -39,4 +40,23 @@ function toggle_git()
     lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua toggle_git()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>lg", ":lua toggle_git()<CR>", { noremap = true, silent = true })
+
+----------------------------------- BASIC ----------------------------------------------
+local terminal = Terminal:new({
+    dir = "git_dir",
+    direction = "float",
+    float_opts = {
+        border = "single",
+    },
+    on_close = function()
+        vim.cmd(":e")
+    end,
+    hidden = true,
+})
+
+function toggle_term()
+    terminal:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>tm", ":lua toggle_term()<CR>", { noremap = true, silent = true })
