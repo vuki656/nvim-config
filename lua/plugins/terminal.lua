708402b39a8e823aa2ddbe004e96be5e09d4c0eb
domagoj.vukovic <off.vukovic@gmail.com>
2021-08-29 17:41:08 +0200

    ref: remove conflicting keys from terminal

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -19,8 +19,7 @@ toggleterm.setup({
 
 function _G.set_terminal_keymaps()
     local opts = { noremap = true }
-    vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+
     vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
     vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
     vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
@@ -30,6 +29,7 @@ end
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 ----------------------------------- LAZYGIT ----------------------------------------------
+
 local lazygit = Terminal:new({
     cmd = "lazygit",
     dir = "git_dir",
@@ -55,6 +55,7 @@ end
 vim.api.nvim_set_keymap("n", "<leader>lg", ":lua toggle_git()<CR>", { noremap = true, silent = true })
 
 ----------------------------------- BASIC ----------------------------------------------
+
 local terminal = Terminal:new({
     dir = "git_dir",
     direction = "float",
