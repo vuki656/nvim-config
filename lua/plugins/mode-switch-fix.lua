fd61448d18fcb2d22655732895d96531d0b24726
domagoj.vukovic <off.vukovic@gmail.com>
2021-10-23 22:41:20 +0200

    fix: entering normal mode with keys delay

---
+++ lua/plugins/mode-switch-fix.lua
@@ -0,0 +1,11 @@
+-- Name: Better escape
+-- Description: Fix delay when typing keys that enter normal mode
+-- Link: https://github.com/max397574/better-escape.nvim
+
+local better_escape = require("better_escape")
+
+------------------------------------------------------------------------------------------
+----------------------------------- SETUP ------------------------------------------------
+------------------------------------------------------------------------------------------
+
+better_escape.setup()
