3496b9cbc3119ca2a4e7a66af50b33ced5c7adfd
domagoj.vukovic <off.vukovic@gmail.com>
2021-11-01 19:57:16 +0100

    fix: insert brackets on completion

--- lua/plugins/auto-pairs.lua
+++ lua/plugins/auto-pairs.lua
@@ -3,9 +3,14 @@
 -- Link: https://github.com/windwp/nvim-autopairs
 
 local autopairs = require("nvim-autopairs")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
 
 ------------------------------------------------------------------------------------------
 ----------------------------------- SETUP ------------------------------------------------
 ------------------------------------------------------------------------------------------
 
 autopairs.setup({})
+
+-- Insert brackes on completion confirm
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
