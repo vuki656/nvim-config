78653256594de9c48c4cc5339eb2e5ec71df6ccd
Domagoj <off.vukovic@gmail.com>
2021-11-29 16:31:25 +0100

    ref: temp disable teal

--- lua/lsp/teal.lua
+++ lua/lsp/teal.lua
@@ -3,21 +3,21 @@ local configs = require("lspconfig/configs")
 
 local capabilities = require("lsp.capabilities")
 
-if not lspconfig.teal then
-    configs.teal = {
-        capabilities = capabilities,
-        default_config = {
-            cmd = {
-                "teal-language-server",
-                -- "logging=on", use this to enable logging in /tmp/teal-language-server.log
-            },
-            filetypes = {
-                "teal",
-            },
-            root_dir = lspconfig.util.root_pattern("tlconfig.lua", ".git"),
-            settings = {},
-        },
-    }
-end
+-- if not lspconfig.teal then
+--     configs.teal = {
+--         capabilities = capabilities,
+--         default_config = {
+--             cmd = {
+--                 "teal-language-server",
+--                 -- "logging=on", use this to enable logging in /tmp/teal-language-server.log
+--             },
+--             filetypes = {
+--                 "teal",
+--             },
+--             root_dir = lspconfig.util.root_pattern("tlconfig.lua", ".git"),
+--             settings = {},
+--         },
+--     }
+-- end
 
-lspconfig.teal.setup({})
+-- lspconfig.teal.setup({})
