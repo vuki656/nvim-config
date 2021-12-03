8f58a486e87c380b33c2478dd9dd8080953f4855
domagoj.vukovic <off.vukovic@gmail.com>
2021-12-03 10:53:18 +0100

    feat: completion source for commands/search

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -39,6 +39,7 @@ return packer.startup(function(use)
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-nvim-lua",
+            "hrsh7th/cmp-cmdline"
         },
     })
     use("jose-elias-alvarez/minsnip.nvim")
