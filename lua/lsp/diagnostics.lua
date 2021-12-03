3db2b80cdc6a78bc699ab9838cec1db26989e44b
domagoj.vukovic <off.vukovic@gmail.com>
2021-12-03 11:59:08 +0100

    fix: diagnostics float colors

--- lua/lsp/diagnostics.lua
+++ lua/lsp/diagnostics.lua
@@ -4,13 +4,6 @@ local colors = require("utils.colors")
 ----------------------------------- SETUP ------------------------------------------------
 ------------------------------------------------------------------------------------------
 
--- TODO: remove
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    signs = false, -- Disable LSP signs
-    underline = true, -- Underline diagnostics
-    virtual_text = false, -- Hide virtual text
-})
-
 vim.diagnostic.config({
     signs = false, -- Disable LSP signs
     underline = true, -- Underline diagnostics
@@ -21,12 +14,14 @@ vim.diagnostic.config({
 ----------------------------------- COLORS -----------------------------------------------
 ------------------------------------------------------------------------------------------
 
-vim.cmd([[highlight DiagnosticDefaultError              guifg=]] .. colors.red)
-vim.cmd([[highlight DiagnosticDefaultWarning            guifg=]] .. colors.yellow)
-vim.cmd([[highlight DiagnosticDefaultInformation        guifg=]] .. colors.blue)
-vim.cmd([[highlight DiagnosticDefaultHint               guifg=]] .. colors.orange)
-
+-- In code colors
 vim.cmd([[highlight DiagnosticUnderlineError            guifg=]] .. colors.red)
 vim.cmd([[highlight DiagnosticUnderlineWarning          guifg=]] .. colors.yellow)
 vim.cmd([[highlight DiagnosticUnderlineInformation      guifg=]] .. colors.blue)
 vim.cmd([[highlight DiagnosticUnderlineHint             guifg=]] .. colors.orange)
+
+-- Float colors
+vim.cmd([[highlight DiagnosticFloatingError            guifg=]] .. colors.red)
+vim.cmd([[highlight DiagnosticFloatingWarn             guifg=]] .. colors.yellow)
+vim.cmd([[highlight DiagnosticFloatingInfo             guifg=]] .. colors.blue)
+vim.cmd([[highlight DiagnosticFloatingHint             guifg=]] .. colors.orange)
