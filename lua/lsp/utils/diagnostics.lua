666bcb233c5a27d32b4eeaefbcda363475156abb
domagoj.vukovic <off.vukovic@gmail.com>
2021-12-19 22:56:21 +0100

    feat: add missing diagnostics icons

--- lua/lsp/utils/diagnostics.lua
+++ lua/lsp/utils/diagnostics.lua
@@ -36,3 +36,11 @@ set_highlight({
         { group = "DiagnosticHint", foreground = colors.orange },
     },
 })
+
+-- Global diagnostic icons
+vim.cmd([[
+    sign define DiagnosticSignError text= texthl=DiagnosticSignError 
+    sign define DiagnosticSignWarn  text= texthl=DiagnosticSignWarn
+    sign define DiagnosticSignInfo  text= texthl=DiagnosticSignInfo
+    sign define DiagnosticSignHint  text= texthl=DiagnosticSignHint 
+]])
