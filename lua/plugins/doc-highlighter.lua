47648d127592c7afcb1af81a38a54869b0c254e4
domagoj.vukovic <off.vukovic@gmail.com>
2021-12-19 19:41:08 +0100

    ref: move headlines plugin to own file

--- lua/plugins/doc-highlighter.lua
+++ lua/plugins/doc-highlighter.lua
@@ -2,8 +2,17 @@
 -- Description: Highlights sections in documentation files
 -- Link: https://github.com/lukas-reineke/headlines.nvim
 
+local headlines = require("headlines")
+
 local set_highlight = require("utils.set_highlight")
 local colors = require("utils.colors")
+
+------------------------------------------------------------------------------------------
+----------------------------------- SETUP ------------------------------------------------
+------------------------------------------------------------------------------------------
+
+-- Doc files header highlights
+headlines.setup()
 
 --------------------------------------------------------------------------------------------
 ------------------------------------- COLORS -----------------------------------------------
