6e2a92555454a7d99e8e8b155cc4af405afe2ead
domagoj.vukovic <off.vukovic@gmail.com>
2022-01-07 20:47:55 +0100

    ref: disable scrollbar until fixed

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,10 +13,11 @@ return require("packer").startup(function(use)
         "dstein64/vim-startuptime",
         config = load_file("profiler"),
     })
-    use({
-        "petertriho/nvim-scrollbar",
-        config = load_file("scrollbar"),
-    })
+    -- TODO: re-enable when fixed
+    -- use({
+    --     "petertriho/nvim-scrollbar",
+    --     config = load_file("scrollbar"),
+    -- })
     use({
         "feline-nvim/feline.nvim",
         config = load_file("status-line"),
