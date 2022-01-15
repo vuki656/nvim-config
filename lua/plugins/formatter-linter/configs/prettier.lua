2c980bf886999f1c5ea6d88cbf398349b0e0a55e
domagoj.vukovic <off.vukovic@gmail.com>
2022-01-15 12:48:00 +0100

    fix: prettier setup

--- lua/plugins/formatter-linter/configs/prettier.lua
+++ lua/plugins/formatter-linter/configs/prettier.lua
@@ -5,40 +5,28 @@ local formatter = null_ls.builtins.formatting
 
 local has_eslint = conditional.root_has_file(".eslintrc.js") or conditional.root_has_file(".eslintrc.json")
 
-local M = {
-    filetypes = {
-        "vue",
-        "css",
-        "html",
-        "yaml",
-        "markdown",
-        "json",
-    },
-    args = {
-        "--stdin-filepath",
-        "$FILENAME",
-    },
+local filetypes = {
+    "vue",
+    "css",
+    "html",
+    "yaml",
+    "markdown",
+    "json",
 }
 
-M.with_eslint = formatter.prettier.with({
-    condition = function()
-        return has_eslint
-    end,
-    filetypes = M.filetypes,
-    args = M.args,
-})
-
-M.standalone = formatter.prettier.with({
-    condition = function()
-        return not has_eslint
-    end,
-    filetypes = vim.tbl_deep_extend("force", {}, M.filetypes, {
+if not has_eslint then
+    filetypes = vim.tbl_deep_extend("force", {}, filetypes, {
         "javascript",
         "javascriptreact",
         "typescript",
         "typescriptreact",
-    }),
-    args = M.args,
-})
+    })
+end
 
-return M
+return formatter.prettier.with({
+    filetypes = filetypes,
+    args = {
+        "--stdin-filepath",
+        "$FILENAME",
+    },
+})
