0b35d5453a25300a771519838be79b8a9a8db531
Domagoj <off.vukovic@gmail.com>
2022-01-26 10:09:41 +0100

    feat: dont jump cursor on reload

--- lua/plugins/terminal/keymaps/reset.lua
+++ lua/plugins/terminal/keymaps/reset.lua
@@ -1,7 +1,11 @@
 return function()
     vim.defer_fn(function()
+        local view = vim.fn.winsaveview()
+
         vim.cmd(":NvimTreeRefresh")
         vim.cmd(":Gitsigns refresh")
         vim.cmd(":e")
+
+        vim.fn.winrestview(view)
     end, 20)
 end
