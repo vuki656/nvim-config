80fee736a4863ed6b045fe789f3898cbbd904cfd
domagoj.vukovic <off.vukovic@gmail.com>
2022-03-07 21:15:32 +0100

    ref: change lsp installer icons

--- lua/plugins/lsp-installer.lua
+++ lua/plugins/lsp-installer.lua
@@ -11,9 +11,9 @@ local lsp_installer = require("nvim-lsp-installer")
 lsp_installer.settings({
     ui = {
         icons = {
-            server_installed = "✓",
-            server_pending = "",
-            server_uninstalled = "✗",
+            server_installed = "",
+            server_pending = "",
+            server_uninstalled = "",
         },
     },
 })
