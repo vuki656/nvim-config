152bd1bf6b3246c37259257f42e0d5e609783225
domagoj.vukovic <off.vukovic@gmail.com>
2022-04-02 19:03:30 +0200

    fix: reloading on non buffers

--- lua/plugins/terminal/keymaps/reset.lua
+++ lua/plugins/terminal/keymaps/reset.lua
@@ -1,11 +1,14 @@
 return function()
     vim.defer_fn(function()
         local view = vim.fn.winsaveview()
+        local buffer_name = vim.fn.bufname()
 
         vim.cmd(":NvimTreeRefresh")
         vim.cmd(":Gitsigns refresh")
-        vim.cmd(":e")
 
-        vim.fn.winrestview(view)
+        if buffer_name ~= "" then
+            vim.cmd(":e")
+            vim.fn.winrestview(view)
+        end
     end, 20)
 end
