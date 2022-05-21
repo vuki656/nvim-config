75873070834824d658a01bcb1b399256cefc2ca7
domagoj.vukovic <off.vukovic@gmail.com>
2022-05-21 11:04:37 +0200

    fix: win separator color

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -94,7 +94,7 @@ set_highlight({
         { group = "PMenuSbar", background = grey_light },
         { group = "PmenuSel", background = green, foreground = background },
         { group = "LineNr", background = background, foreground = grey },
-        { group = "VertSplit", background = background_light, foreground = background },
+        { group = "WinSeparator", background = background, foreground = grey_light },
         { group = "Visual", background = grey_light },
         { group = "MatchParen", foreground = red, background = background },
         { group = "ErrorMsg", foreground = red, background = background },
