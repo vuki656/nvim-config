d72635181bbac0ee0e2e3965345abbf938a2929b
domagoj.vukovic <off.vukovic@gmail.com>
2022-09-01 21:53:23 +0200

    ref: unpeg autosave

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -2,16 +2,17 @@
 -- Description: Auto save buffer
 -- Link: https://github.com/Pocco81/auto-save.nvim
 
-local autosave = require("autosave")
+local autosave = require("auto-save")
 
 ------------------------------------------------------------------------------------------
 ----------------------------------- SETUP ------------------------------------------------
 ------------------------------------------------------------------------------------------
 
 autosave.setup({
-    execution_message = "",
-    events = {
-        "InsertLeave",
-        "TextChanged",
+    execution_message = {
+        message = function()
+            return ""
+        end,
     },
+    trigger_events = { "InsertLeave", "TextChanged" },
 })
