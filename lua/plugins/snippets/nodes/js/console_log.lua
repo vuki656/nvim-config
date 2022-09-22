9d75e2190878021964a032c3162a1a6287e35add
domagoj.vukovic <off.vukovic@gmail.com>
2022-09-22 22:27:00 +0200

    fix: console log snippet

--- lua/plugins/snippets/nodes/js/console_log.lua
+++ lua/plugins/snippets/nodes/js/console_log.lua
@@ -1,8 +1,7 @@
 local luasnip = require("luasnip")
 local format = require("luasnip.extras.fmt").fmt
-local repeat_node = require("luasnip.extras").rep
 
 local create_snippet = luasnip.snippet
 local insert_node = luasnip.insert_node
 
-return create_snippet("clg", format("console.log('{}', {})", { repeat_node(1), insert_node(1) }))
+return create_snippet("clg", format("console.log({})", { insert_node(1) }))
