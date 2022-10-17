e8d603f93f78c0964f894e074f9249054b3e513c
domagoj.vukovic <off.vukovic@gmail.com>
2022-10-18 00:22:27 +0200

    fix: doc

--- lua/utils/load-plugin-file.lua
+++ lua/utils/load-plugin-file.lua
@@ -1,5 +1,5 @@
 --- Sources the given file name in the plugins folder
--- @param options - object
+-- @param name - string
 return function(name)
     return "require('plugins." .. name .. "')"
 end
