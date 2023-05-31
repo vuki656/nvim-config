2ecf51e873bde45ef4660a3361f1e832e78fd816
domagoj.vukovic <off.vukovic@gmail.com>
2023-05-31 18:25:49 +0200

    ref: use lua setup for scrollbar plugin

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -3,20 +3,25 @@
 -- Link: https://github.com/dstein64/nvim-scrollview
 
 local colors = require("utils.colors")
+local scrollview = require("scrollview")
 local set_highlight = require("utils.set-highlight")
 
 ------------------------------------------------------------------------------------------
 ----------------------------------- CONFIG -----------------------------------------------
 ------------------------------------------------------------------------------------------
 
--- Don't display in following filetypes
-vim.g.scrollview_excluded_filetypes = {
-    "NvimTree",
-    "alpha",
-}
+scrollview.setup({
+    excluded_filetypes = {
+        "alpha",
+        "NvimTree",
+    },
+})
 
 ------------------------------------------------------------------------------------------
 ----------------------------------- COLORS -----------------------------------------------
 ------------------------------------------------------------------------------------------
 
-set_highlight({ group = "ScrollView", background = colors.background_light })
+set_highlight({
+    group = "ScrollView",
+    background = colors.background_light,
+})
