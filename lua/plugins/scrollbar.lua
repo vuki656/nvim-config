30be61bf06c42c0fd670047dbfb3d505b3932f2b
domagoj.vukovic <off.vukovic@gmail.com>
2023-06-02 22:13:26 +0200

    feat: custom signs for scrollbar

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -2,6 +2,7 @@
 -- Description: Scrollbar
 -- Link: https://github.com/dstein64/nvim-scrollview
 
+local LSP_ICONS = require("lsp.utils.icons")
 local colors = require("utils.colors")
 local scrollview = require("scrollview")
 local set_highlight = require("utils.set-highlight")
@@ -15,6 +16,10 @@ scrollview.setup({
         "alpha",
         "NvimTree",
     },
+    signs_max_per_row = 1,
+    search_symbol = "",
+    diagnostics_warn_symbol = LSP_ICONS.WARNING,
+    diagnostics_error_symbol = LSP_ICONS.ERROR,
 })
 
 ------------------------------------------------------------------------------------------
