c797ab03b80c6f38d28ef8671d0fb12582bbbdb7
domagoj.vukovic <off.vukovic@gmail.com>
2023-07-13 22:30:05 +0200

    fix: ignore some vim notify messages

--- lua/plugins/notifications.lua
+++ lua/plugins/notifications.lua
@@ -17,8 +17,18 @@ notify.setup({
     render = "minimal",
 })
 
--- Pipe all vim.notify notifications to notify plugin
-vim.notify = notify
+local banned_messages = { "No information available" }
+
+-- Pipe all vim.notify notifications to notify plugin but ignore some
+vim.notify = function(message, ...)
+    for _, banned in ipairs(banned_messages) do
+        if message == banned then
+            return
+        end
+    end
+
+    return notify(message, ...)
+end
 
 ------------------------------------------------------------------------------------------
 ----------------------------------- COLORS -----------------------------------------------
