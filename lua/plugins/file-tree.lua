2ef51e8a03ce9bf90b869823ebe5f822d7922cd2
domagoj.vukovic <off.vukovic@gmail.com>
2023-08-14 22:21:59 +0200

    fix: migrate to new nvim tree keymap setup

--- lua/plugins/file-tree.lua
+++ lua/plugins/file-tree.lua
@@ -12,14 +12,44 @@ local set_keymap = require("utils.set-keymap")
 ----------------------------------- CONFIG -----------------------------------------------
 ------------------------------------------------------------------------------------------
 
+-- TODO: clean up
+local function on_attach(bufnr)
+    local api = require("nvim-tree.api")
+
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    vim.keymap.set("n", "<C-]>", api.tree.change_root_to_node, opts("CD"))
+    vim.keymap.set("n", "-", api.tree.change_root_to_parent, opts("Up"))
+    vim.keymap.set("n", "<C-k>", api.node.show_info_popup, opts("Info"))
+    vim.keymap.set("n", "<C-r>", api.fs.rename_sub, opts("Rename: Omit Filename"))
+    vim.keymap.set("n", "<BS>", api.node.navigate.parent_close, opts("Close Directory"))
+    vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", ".", api.node.run.cmd, opts("Run Command"))
+    vim.keymap.set("n", "a", api.fs.create, opts("Create"))
+    vim.keymap.set("n", "c", api.fs.copy.node, opts("Copy"))
+    vim.keymap.set("n", "e", api.fs.rename_basename, opts("Rename: Basename"))
+    vim.keymap.set("n", "g?", api.tree.toggle_help, opts("Help"))
+    vim.keymap.set("n", "gy", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+    vim.keymap.set("n", "I", api.tree.toggle_gitignore_filter, opts("Toggle Filter: Git Ignore"))
+    vim.keymap.set("n", "p", api.fs.paste, opts("Paste"))
+    vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
+    vim.keymap.set("n", "R", api.tree.reload, opts("Refresh"))
+    vim.keymap.set("n", "W", api.tree.collapse_all, opts("Collapse"))
+    vim.keymap.set("n", "x", api.fs.cut, opts("Cut"))
+    vim.keymap.set("n", "<2-LeftMouse>", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "<2-RightMouse>", api.tree.change_root_to_node, opts("CD"))
+    vim.keymap.set("n", "bd", api.marks.bulk.delete, opts("Delete Bookmarked"))
+    vim.keymap.set("n", "bmv", api.marks.bulk.move, opts("Move Bookmarked"))
+    vim.keymap.set("n", "m", api.marks.toggle, opts("Toggle Bookmark"))
+    vim.keymap.set("n", "d", api.fs.trash, opts("Trash"))
+end
+
 tree.setup({
+    on_attach = on_attach,
     view = {
         width = 40,
-        mappings = {
-            list = {
-                { key = "d", action = "trash" },
-            },
-        },
     },
     respect_buf_cwd = true,
     renderer = {
