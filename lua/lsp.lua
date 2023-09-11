125ebd533525ff1e2d312db02d805e562b77a193
domagoj.vukovic <off.vukovic@gmail.com>
2023-09-11 18:33:28 +0200

    fix: attempt to fix semantic tokens error

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -138,3 +138,10 @@ typescript.setup({
         separate_diagnostic_server = true,
     },
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        client.server_capabilities.semanticTokensProvider = nil
+    end,
+})
