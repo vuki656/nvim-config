91f2e7d9d4b915aee92658817358a05730907c94
domagoj.vukovic <off.vukovic@gmail.com>
2023-09-14 14:43:40 +0200

    refactor: remove unused snippets

--- lua/plugins/snippets/init.lua
+++ lua/plugins/snippets/init.lua
@@ -12,26 +12,13 @@ local arrow_function_full_async_snippet = require("plugins.snippets.nodes.js.arr
 local arrow_function_full_snippet = require("plugins.snippets.nodes.js.arrow_function_full")
 local arrow_function_snippet = require("plugins.snippets.nodes.js.arrow_function")
 local console_log_snippet = require("plugins.snippets.nodes.js.console_log")
-local console_log_stringify_snippet = require("plugins.snippets.nodes.js.console_log_stringify")
 local export_all_snippet = require("plugins.snippets.nodes.js.export_all")
-local service_function_snippet = require("plugins.snippets.nodes.js.service_function")
 local type_snippet = require("plugins.snippets.nodes.js.type")
 
 -- React
 local react_component_props_snippet = require("plugins.snippets.nodes.js.react.react_component_props")
 local react_component_snippet = require("plugins.snippets.nodes.js.react.react_component")
 local react_element_snippet = require("plugins.snippets.nodes.js.react.react_element")
-
--- Styled Components
-local styled_component = require("plugins.snippets.nodes.js.styled_components.styled_component")
-local styled_component_import = require("plugins.snippets.nodes.js.styled_components.styled_component_import")
-local styled_component_props = require("plugins.snippets.nodes.js.styled_components.styled_component_props")
-
--- Jest
-local jest_suite = require("plugins.snippets.nodes.js.jest.suite")
-
--- Cypress
-local cypress_function = require("plugins.snippets.nodes.js.cypress.function")
 
 ------------------------------------------------------------------------------------------
 ------------------------------------ SETUP -----------------------------------------------
@@ -49,29 +36,17 @@ luasnip.config.set_config({
 local js_ts_snippets = {
     -- Generic JS/TS
     console_log_snippet,
-    console_log_stringify_snippet,
     export_all_snippet,
     arrow_function_snippet,
     arrow_function_async_snippet,
     arrow_function_full_async_snippet,
     arrow_function_full_snippet,
-    service_function_snippet,
     type_snippet,
-    jest_suite,
-
-    -- Cypress
-    cypress_function,
-
-    -- Styled Components
-    styled_component,
-    styled_component_props,
-    styled_component_import,
 }
 
 local js_ts_react_snippets = {
     -- Generic JS/TS
     console_log_snippet,
-    console_log_stringify_snippet,
     export_all_snippet,
     arrow_function_snippet,
     arrow_function_full_snippet,
