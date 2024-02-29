17982e8913e5ef836d69e3aceab8372228ad4546
Domagoj Vukovic <off.vukovic@gmail.com>
2024-02-29 21:33:03 +0100

    feat: try no comparators

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -76,13 +76,13 @@ cmp.setup({
     },
     sorting = {
         comparators = {
-            cmp.config.compare.offset,
-            cmp.config.compare.exact,
-            cmp.config.compare.score,
-            cmp.config.compare.kind,
-            cmp.config.compare.sort_text,
-            cmp.config.compare.length,
-            cmp.config.compare.order,
+            -- cmp.config.compare.offset,
+            -- cmp.config.compare.exact,
+            -- cmp.config.compare.score,
+            -- cmp.config.compare.kind,
+            -- cmp.config.compare.sort_text,
+            -- cmp.config.compare.length,
+            -- cmp.config.compare.order,
         },
     },
 })
