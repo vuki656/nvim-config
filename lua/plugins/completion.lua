8e228539150060557afb806ae2df14d2611a52bf
Domagoj Vukovic <off.vukovic@gmail.com>
2024-02-29 21:19:35 +0100

    feat: new completion comparator

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -73,8 +73,10 @@ cmp.setup({
             cmp.config.compare.offset,
             cmp.config.compare.exact,
             cmp.config.compare.score,
-            cmp.config.compare.recently_used,
             cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
         },
     },
 })
