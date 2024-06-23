1e71ae79c91dde2a00b2b6c94fadedad675af5a4
Domagoj Vukovic <off.vukovic@gmail.com>
2024-06-24 00:42:42 +0200

    feat: remove dadbod

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -64,7 +64,6 @@ cmp.setup({
         format = icons.cmp_format(),
     },
     sources = {
-        { name = "vim-dadbod-completion" },
         { name = "nvim_lsp" },
         { name = "luasnip" },
         {
