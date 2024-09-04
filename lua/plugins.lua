7577cd52b3adb60645b198aebf8ce463ca1ef1fe
Domagoj Vukovic <off.vukovic@gmail.com>
2024-09-04 15:21:27 +0200

    feat: copilot custom keymap

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,6 +3,14 @@ local config = require("plugins.plugin-manager")
 require("lazy").setup({
     {
         "github/copilot.vim",
+        config = function()
+            vim.g.copilot_no_tab_map = true
+
+            vim.keymap.set("i", "<C-o>", 'copilot#Accept("\\<CR>")', {
+                expr = true,
+                replace_keycodes = false,
+            })
+        end,
     },
     {
         "mistweaverco/kulala.nvim",
