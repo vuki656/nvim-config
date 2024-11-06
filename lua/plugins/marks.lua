6692450fe4fc5c2d8ddcfd0a64eec4e107c18a6c
Domagoj Vukovic <off.vukovic@gmail.com>
2024-11-06 21:14:42 +0100

    feat: add keymap to remove mark on current line

--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -1,6 +1,6 @@
 -- Name: Marks
 -- Description: Better marks ui and utils
--- Link: https://github.com/chentau/marks.nvim
+-- Link: https://github.com/chentoast/marks.nvim
 
 local marks = require("marks")
 
@@ -17,10 +17,21 @@ marks.setup({})
 ------------------------------------------------------------------------------------------
 
 set_keymap({
-    key = "<LEADER>rm",
-    actions = function()
-        vim.cmd("delmarks!")
-        vim.cmd("wshada!")
-    end,
-    description = "Delete all marks in the current buffer",
+    list = {
+        {
+            key = "<LEADER>rma",
+            actions = function()
+                vim.cmd("delmarks!")
+                vim.cmd("wshada!")
+            end,
+            description = "Delete all marks in the current buffer",
+        },
+        {
+            key = "<LEADER>rml",
+            actions = function()
+                vim.api.nvim_input("dm-")
+            end,
+            description = "Delete mark on the current line",
+        },
+    },
 })
