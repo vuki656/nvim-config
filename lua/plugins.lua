ada450b06c6f332d3abcb16adec92f1537938748
Domagoj Vukovic <off.vukovic@gmail.com>
2025-01-08 21:46:08 +0100

    fix: git conflict keymaps

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -118,6 +118,7 @@ require("lazy").setup({
     },
     {
         "akinsho/git-conflict.nvim",
+        tag = 'v2.1.0',
         config = function()
             require("plugins.git-conflict")
         end,
