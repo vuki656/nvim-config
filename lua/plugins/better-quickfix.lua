630ea66bd8f139d03631e5ee0bc048fe73b1e55c
Domagoj Vukovic <off.vukovic@gmail.com>
2026-01-24 11:57:40 +0100

    feat: delete from qf list

--- lua/plugins/better-quickfix.lua
+++ lua/plugins/better-quickfix.lua
@@ -9,3 +9,27 @@ local list = require("pqf")
 ------------------------------------------------------------------------------------------
 
 list.setup()
+
+------------------------------------------------------------------------------------------
+----------------------------------- KEYMAPS ----------------------------------------------
+------------------------------------------------------------------------------------------
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "qf",
+    callback = function()
+        vim.keymap.set("n", "dd", function()
+            local local_list = vim.fn.getqflist()
+            local ids = vim.fn.line(".")
+
+            table.remove(local_list, ids)
+
+            vim.fn.setqflist(local_list, "r")
+
+            local new_ids = math.min(ids, #local_list)
+
+            if new_ids > 0 then
+                vim.fn.cursor(new_ids, 1)
+            end
+        end, { buffer = true, desc = "Remove quickfix entry" })
+    end,
+})
